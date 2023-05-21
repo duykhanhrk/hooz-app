466e1cbc683304fc60d6dac32641f9dd0d15c7b8
duykhanhrk <tranduykhanh.rk@gmail.com>
2023-05-21 23:19:07 +0700

    feat: all

--- screens/ReadingScreen.tsx
+++ screens/ReadingScreen.tsx
@@ -2,13 +2,14 @@ import {useQuery} from "@tanstack/react-query";
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RouteProp} from '@react-navigation/native';
-import {useMemo} from 'react';
+import {useEffect, useMemo} from 'react';
 import {AppStackParamList} from "@navigation";
 import {BookService} from "@services";
 import LoadingScreen from "../components/LoadingScreen";
 import ErrorScreen from "../components/ErrorScreen";
 import WebView from "react-native-webview";
 import useAppSelector from "../hooks/useAppSelector";
+import {AxiosError} from "axios";
 
 export default function PolicyAndTermScreen() {
   const readingOption = useAppSelector(state => state.readingOption);
@@ -17,10 +18,19 @@ export default function PolicyAndTermScreen() {
   const route = useRoute<RouteProp<AppStackParamList, 'ReadingScreen'>>();
   const {book_id, chapter_id} = route.params;
 
-  const query = useQuery({
+  const query = useQuery<any, AxiosError>({
     queryKey: ['book', book_id, 'chapter', chapter_id],
-    queryFn: () => BookService.getChapterAsync(chapter_id)
+    queryFn: () => BookService.getChapterAsync(chapter_id),
+    retry: false
   })
+
+  useEffect(() => {
+    const willFocusSubscription = navigation.addListener('focus', () => {
+      query.refetch();
+    });
+
+    return willFocusSubscription;
+  }, [navigation]);
 
   const html = useMemo(
     () => `<div style="padding: 0px 8px; color: ${readingOption.color}; font-size: ${readingOption.fontSize}px; font-weight: ${readingOption.fontWeight}; text-align: ${readingOption.textAlign}">${query.data ? query.data.chapter.content : ''}</div>`,
@@ -32,7 +42,23 @@ export default function PolicyAndTermScreen() {
   }
 
   if (query.isError) {
-    return <ErrorScreen />
+    if (query.error.response && query.error.response.status == 403) {
+      return (
+        <ErrorScreen
+          messages={['Bạn cần mua gói để sử dụng tài nguyên này']}
+          buttonText={'Mua gói ngay'}
+          onButtonPress={() => navigation.navigate('PlansScreen')}
+        />
+      )
+    }
+
+    return (
+      <ErrorScreen
+        onButtonPress={() => {
+          query.refetch();
+        }}
+      />
+    )
   }
 
   return (
